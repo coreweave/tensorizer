051fe555a933bf92c2fc93e37f104a3574075ac8
Eta <esyra@coreweave.com>
2024-01-31 19:37:20 -0600

    feat(examples): Support S3 for all `StableDiffusionPipeline` components
    This additionally adds parameter weight validation for diffusers
    models, refactors `serialize_model` substantially, adds logging level
    command line arguments, and shifts more outputs to use a logger.

--- examples/hf_serialization.py
+++ examples/hf_serialization.py
@@ -5,14 +5,15 @@ import logging
 import os
 import tempfile
 import time
+import zipfile
 from functools import partial
+from pathlib import Path
 from typing import Optional, Type, Union
 
 import torch
 from diffusers import (
     AutoencoderKL,
     ConfigMixin,
-    LMSDiscreteScheduler,
     ModelMixin,
     StableDiffusionPipeline,
     UNet2DConditionModel,
@@ -20,11 +21,11 @@ from diffusers import (
 from transformers import (
     AutoConfig,
     AutoModelForCausalLM,
-    AutoTokenizer,
     CLIPTextConfig,
     CLIPTextModel,
     PretrainedConfig,
     PreTrainedModel,
+    PreTrainedTokenizer,
 )
 
 from tensorizer import TensorDeserializer, TensorSerializer, stream_io, utils
@@ -33,35 +34,78 @@ s3_access_key_id = os.environ.get("S3_ACCESS_KEY_ID") or None
 s3_secret_access_key = os.environ.get("S3_SECRET_ACCESS_KEY") or None
 s3_endpoint = os.environ.get("S3_ENDPOINT_URL") or None
 
-_read_stream = partial(
-    stream_io.open_stream,
-    mode="rb",
-    s3_access_key_id=s3_access_key_id,
-    s3_secret_access_key=s3_secret_access_key,
-    s3_endpoint=s3_endpoint,
-)
-
-_write_stream = partial(
-    stream_io.open_stream,
-    mode="wb+",
-    s3_access_key_id=s3_access_key_id,
-    s3_secret_access_key=s3_secret_access_key,
-    s3_endpoint=s3_endpoint,
+_read_stream, _write_stream = (
+    partial(
+        stream_io.open_stream,
+        mode=mode,
+        s3_access_key_id=s3_access_key_id,
+        s3_secret_access_key=s3_secret_access_key,
+        s3_endpoint=s3_endpoint,
+    )
+    for mode in ("rb", "wb+")
 )
 
 
-# Setup logger
-logger = logging.getLogger(__name__)
-logger.setLevel(level=logging.INFO)
-fh = logging.StreamHandler()
-fh_formatter = logging.Formatter(
-    "%(asctime)s %(levelname)s %(filename)s(%(process)d) - %(message)s"
-)
-fh.setFormatter(fh_formatter)
-logger.addHandler(fh)
+def setup_logger():
+    _logger = logging.getLogger(__name__)
+    _logger.setLevel(level=logging.INFO)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(asctime)s %(levelname)s %(filename)s(%(process)d) - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    _logger.addHandler(handler)
+    return _logger
 
 
-def check_file_exists(file: str):
+logger = setup_logger()
+
+
+def set_logger_verbosity(verbosity: int = 2):
+    """
+    Configure the verbosity of the global logger.
+    Args:
+        verbosity: Verbosity level. Clamped to [0, 3].
+    """
+    levels = (logging.CRITICAL, logging.WARNING, logging.INFO, logging.DEBUG)
+    verbosity: int = min(len(levels) - 1, max(0, verbosity))
+    logger.setLevel(levels[verbosity])
+
+
+class ModuleValidationError(RuntimeError):
+    pass
+
+
+def assert_module_equal(
+    before: torch.nn.Module, after: torch.nn.Module, label: Optional[str] = None
+) -> None:
+    """
+    Check that the state dicts of two modules are equal.
+    Args:
+        before: The original module to compare against.
+        after: The secondary module to compare.
+        label: Optional label to include in error messages.
+
+    Raises:
+        ModuleValidationError: If the modules do not match.
+    """
+    label: str = f"{label}: " if label else ""
+    before_sd = before.state_dict()
+    after_sd = after.state_dict()
+    if before_sd.keys() != after_sd.keys():
+        raise ModuleValidationError(
+            f"{label}Validating deserialized model failed: different keys"
+        )
+    for name, before_tensor in before_sd.items():
+        after_tensor = after_sd[name]
+        if not torch.equal(before_tensor, after_tensor):
+            raise ModuleValidationError(
+                f"{label}Validating deserialized model failed:"
+                f" weights for tensor {name} don't match"
+            )
+
+
+def file_is_non_empty(file: str):
     """
     Check if file exists and is not empty. If the file is found locally,
     it is checked for emptiness with `os.stat`. If the file is found
@@ -86,7 +130,7 @@ def check_file_exists(file: str):
 
 def serialize_model(
     model: torch.nn.Module,
-    config: Optional[Union[ConfigMixin, AutoConfig, dict]],
+    config: Union[ConfigMixin, AutoConfig, dict],
     model_directory: str,
     model_prefix: str = "model",
     force: bool = False,
@@ -98,9 +142,7 @@ def serialize_model(
 
     Args:
         model: The model to serialize.
-        config: The model's configuration. This is optional and only
-            required for HuggingFace Transformers models. Diffusers
-            models do not require this.
+        config: The model's configuration.
         model_directory: The directory to save the serialized model to.
         model_prefix: The prefix to use for the serialized model files. This
             is purely optional, and it allows for multiple models to be
@@ -110,27 +152,29 @@ def serialize_model(
             even if they already exist
     """
 
-    dir_prefix = f"{model_directory}/{model_prefix}"
-    config_file_exists, weights_file_exists = (
-        check_file_exists(f"{dir_prefix}-config.json"),
-        check_file_exists(f"{dir_prefix}.tensors"),
-    )
-    overwrite = force or not all([config_file_exists, weights_file_exists])
-    if overwrite:
-        if config:
-            config_path = f"{dir_prefix}-config.json"
-            logger.info(f"Writing config to {config_path}")
-            with _write_stream(config_path) as f:
-                config_dict = (
-                    config.to_dict() if hasattr(config, "to_dict") else config
-                )
-                f.write(json.dumps(config_dict, indent=2).encode("utf-8"))
-        if model:
-            logger.info(f"Writing tensors to {dir_prefix}.tensors")
-            with _write_stream(f"{dir_prefix}.tensors") as f:
-                ts = TensorSerializer(f)
-                ts.write_module(model, include_non_persistent_buffers=False)
-                ts.close()
+    dir_prefix: str = f"{model_directory}/{model_prefix}"
+    config_path: str = f"{dir_prefix}-config.json"
+    model_path: str = f"{dir_prefix}.tensors"
+    paths = (config_path, model_path)
+    write: bool = force or any(not file_is_non_empty(path) for path in paths)
+    if write:
+        logger.info(f"Writing config to {config_path}")
+        with _write_stream(config_path) as f:
+            config_dict = (
+                config.to_dict() if hasattr(config, "to_dict") else config
+            )
+            f.write(json.dumps(config_dict, indent=2).encode("utf-8"))
+        logger.info(f"Writing tensors to {model_path}")
+        with _write_stream(model_path) as f:
+            serializer = TensorSerializer(f)
+            serializer.write_module(model, include_non_persistent_buffers=False)
+            serializer.close()
+    else:
+        logger.warning(
+            "Skipping serialization because files already"
+            f" exist at {' & '.join(paths)}."
+            " Use the --force option to force an overwrite instead."
+        )
 
 
 def load_model(
@@ -143,8 +187,7 @@ def load_model(
     ] = None,
     model_prefix: Optional[str] = "model",
     device: torch.device = utils.get_device(),
-    dtype: Optional[str] = None,
-    hf_id: Optional[str] = None,
+    dtype: Optional[torch.dtype] = None,
 ) -> torch.nn.Module:
     """
     Given a path prefix, load the model with a custom extension
@@ -166,32 +209,33 @@ def load_model(
     begin_load = time.perf_counter()
     ram_usage = utils.get_mem_usage()
 
-    config_uri = f"{path_uri}/{model_prefix}-config.json"
-    tensors_uri = f"{path_uri}/{model_prefix}.tensors"
+    path_uri: str = path_uri.rstrip("/")
+    config_uri: str = f"{path_uri}/{model_prefix}-config.json"
+    tensors_uri: str = f"{path_uri}/{model_prefix}.tensors"
 
     logger.info(f"Loading {tensors_uri}, {ram_usage}")
 
-    if config_class is not None:
-        try:
-            with tempfile.TemporaryDirectory() as temp_dir:
-                temp_config_path = os.path.join(temp_dir, "config.json")
-                with open(temp_config_path, "wb") as temp_config:
-                    logger.info(f"Loading {config_uri}")
-                    with _read_stream(config_uri) as config_file:
-                        temp_config.write(config_file.read())
-                config = config_class.from_pretrained(temp_dir)
-                config.gradient_checkpointing = True
-        except ValueError:
-            config = config_class.from_pretrained(config_uri)
-        with utils.no_init_or_tensor():
-            # AutoModels instantiate from a config via their from_config()
-            # method, while other classes can usually be instantiated directly.
-            config_loader = getattr(model_class, "from_config", model_class)
-            model = config_loader(config)
+    if config_class is None:
+        config_loader = model_class.load_config
     else:
-        model = model_class.from_config(
-            model_class.load_config(hf_id, subfolder=model_prefix)
-        )
+        config_loader = config_class.from_pretrained
+    try:
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_config_path = os.path.join(temp_dir, "config.json")
+            with open(temp_config_path, "wb") as temp_config:
+                logger.info(f"Loading {config_uri}")
+                with _read_stream(config_uri) as config_file:
+                    temp_config.write(config_file.read())
+            config = config_loader(temp_dir)
+            if isinstance(config, PretrainedConfig):
+                config.gradient_checkpointing = True
+    except ValueError:
+        config = config_loader(config_uri)
+    with utils.no_init_or_tensor():
+        # AutoModels instantiate from a config via their from_config()
+        # method, while other classes can usually be instantiated directly.
+        model_loader = getattr(model_class, "from_config", model_class)
+        model = model_loader(config)
 
     with _read_stream(tensors_uri) as tensor_stream, TensorDeserializer(
         tensor_stream, device=device, dtype=dtype
@@ -210,13 +254,46 @@ def load_model(
     return model
 
 
+def serialize_pretrained(
+    component, path_uri: str, prefix: str, force: bool = False
+):
+    save_path: str = f"{path_uri.rstrip('/')}/{prefix}.zip"
+    if force or not file_is_non_empty(save_path):
+        with _write_stream(save_path) as stream, zipfile.ZipFile(
+            stream, mode="w", compression=zipfile.ZIP_DEFLATED, compresslevel=5
+        ) as file, tempfile.TemporaryDirectory() as directory:
+            component.save_pretrained(directory)
+            for path in Path(directory).iterdir():
+                file.write(filename=path, arcname=path.name)
+    else:
+        logger.warning(
+            "Skipping saving non-tensor component because files already"
+            f" exist at {save_path}."
+            " Use the --force option to force an overwrite instead."
+        )
+
+
+def load_pretrained(
+    component_class: Union[Type[PreTrainedTokenizer], Type[ConfigMixin]],
+    path_uri: str,
+    prefix: str,
+):
+    load_path: str = f"{path_uri.rstrip('/')}/{prefix}.zip"
+    with _read_stream(load_path) as stream, zipfile.ZipFile(
+        stream, mode="r"
+    ) as file, tempfile.TemporaryDirectory() as directory:
+        file.extractall(path=directory)
+        return component_class.from_pretrained(directory, local_files_only=True)
+
+
 def df_main(args: argparse.Namespace) -> None:
     output_prefix = args.output_prefix
-    print("MODEL PATH:", args.input_directory)
-    print("OUTPUT PREFIX:", output_prefix)
+    output_prefix = output_prefix.rstrip("/")
+
+    logger.info(f"MODEL PATH: {args.input_directory}")
+    logger.info(f"OUTPUT PREFIX: {output_prefix}")
 
     hf_api_token = os.environ.get("HF_API_TOKEN")
-    hf_id = args.input_directory
 
     pipeline = StableDiffusionPipeline.from_pretrained(
         args.input_directory, use_auth_token=hf_api_token
@@ -226,54 +303,70 @@ def df_main(args: argparse.Namespace) -> None:
     logger.info("GPU: " + utils.get_gpu_name())
     logger.info("PYTHON USED RAM: " + utils.get_mem_usage())
 
-    serialize_model(
-        pipeline.text_encoder.eval(),
-        pipeline.text_encoder.config,
-        output_prefix,
-        "encoder",
-    )
-    serialize_model(pipeline.vae.eval(), None, output_prefix, "vae")
-    serialize_model(pipeline.unet.eval(), None, output_prefix, "unet")
+    for prefix, component in (
+        ("encoder", pipeline.text_encoder),
+        ("vae", pipeline.vae),
+        ("unet", pipeline.unet),
+    ):
+        component.eval()
+        config = component.config
+        if isinstance(config, dict):
+            config = {k: v for k, v in config.items() if k != "_name_or_path"}
+        serialize_model(
+            component, config, output_prefix, prefix, force=args.force
+        )
 
-    pipeline.tokenizer.save_pretrained(output_prefix)
-    pipeline.scheduler.save_pretrained(output_prefix)
+    serialize_pretrained(
+        pipeline.tokenizer, output_prefix, "tokenizer", force=args.force
+    )
+    serialize_pretrained(
+        pipeline.scheduler, output_prefix, "scheduler", force=args.force
+    )
+    tokenizer_type = type(pipeline.tokenizer)
+    scheduler_type = type(pipeline.scheduler)
 
     if args.validate:
-        del pipeline
-        gc.collect()
         device = utils.get_device()
+        pipeline = pipeline.to(device)
+        gc.collect()
 
         logger.info("Validating serialization")
         vae = load_model(
-            output_prefix, AutoencoderKL, None, "vae", device, hf_id=hf_id
+            output_prefix,
+            AutoencoderKL,
+            None,
+            "vae",
+            device,
         )
+        assert_module_equal(pipeline.vae, vae, "vae")
+
         unet = load_model(
             output_prefix,
             UNet2DConditionModel,
             None,
             "unet",
             device,
-            hf_id=hf_id,
         )
+        assert_module_equal(pipeline.unet, unet, "unet")
+
         encoder = load_model(
             output_prefix,
             CLIPTextModel,
             CLIPTextConfig,
             "encoder",
             device,
-            hf_id=hf_id,
         )
+        assert_module_equal(pipeline.text_encoder, encoder, "encoder")
 
-        pipeline = StableDiffusionPipeline(
+        tokenizer = load_pretrained(tokenizer_type, output_prefix, "tokenizer")
+        scheduler = load_pretrained(scheduler_type, output_prefix, "scheduler")
+
+        deserialized_pipeline = StableDiffusionPipeline(
             text_encoder=encoder,
             vae=vae,
             unet=unet,
-            tokenizer=AutoTokenizer.from_pretrained(
-                args.input_directory, subfolder="tokenizer"
-            ),
-            scheduler=LMSDiscreteScheduler.from_pretrained(
-                args.input_directory, subfolder="scheduler"
-            ),
+            tokenizer=tokenizer,
+            scheduler=scheduler,
             safety_checker=None,
             feature_extractor=None,
             requires_safety_checker=False,
@@ -281,16 +374,15 @@ def df_main(args: argparse.Namespace) -> None:
 
         prompt = "a photo of an astronaut riding a horse on mars"
         with torch.autocast(device.type):
-            pipeline(prompt).images[0].save("test.png")
+            deserialized_pipeline(prompt).images[0].save("test.png")
 
 
 def hf_main(args):
     output_prefix = args.output_prefix
-
     output_prefix = output_prefix.rstrip("/")
 
-    print("MODEL PATH:", args.input_directory)
-    print("OUTPUT PREFIX:", output_prefix)
+    logger.info(f"MODEL PATH: {args.input_directory}")
+    logger.info(f"OUTPUT PREFIX: {output_prefix}")
 
     dtype = torch.float16
     model_config = AutoConfig.from_pretrained(args.input_directory)
@@ -305,15 +397,9 @@ def hf_main(args):
     serialize_model(model, model_config, output_prefix, force=args.force)
 
     if args.validate:
-        # Not sure if this part is needed as, although I doubt it,
-        # I'm not absolutely certain whether or not TensorSerializer
-        # changes the model's state in any way
-        del model
-        gc.collect()
         device = utils.get_device()
-        model = AutoModelForCausalLM.from_pretrained(
-            args.input_directory, config=model_config, torch_dtype=dtype
-        ).to(device)
+        model = model.to(device)
+        gc.collect()
         logger.info("Validating serialization")
         tensorizer_model = load_model(
             output_prefix,
@@ -325,26 +411,14 @@ def hf_main(args):
         ).eval()
         # Comparing model parameters
         logger.debug("Testing for sameness of model parameters")
-        original_sd = model.state_dict()
-        tensorizer_sd = tensorizer_model.state_dict()
-        if tensorizer_sd.keys() != original_sd.keys():
-            raise RuntimeError(
-                "Validating deserialized model failed: different keys"
-            )
-        for name, original_tensor in original_sd.items():
-            tensorizer_tensor = tensorizer_sd[name]
-            if not torch.equal(original_tensor, tensorizer_tensor):
-                raise RuntimeError(
-                    "Validating deserialized model failed:"
-                    f" weights for tensor {name} don't match"
-                )
+        assert_module_equal(model, tensorizer_model)
 
 
 def main(argv=None) -> None:
     parser = argparse.ArgumentParser(
         description=(
             "An example script that uses Tensorizer to serialize"
-            " a HuggingFace model to an output directory."
+            " a HuggingFace model to an output directory or object storage."
         )
     )
     parser.add_argument(
@@ -372,9 +446,25 @@ def main(argv=None) -> None:
         action="store_true",
         help="Overwrite existing serialized tensors in the output directory",
     )
+    parser.add_argument(
+        "-q",
+        "--quiet",
+        action="count",
+        help="Show less output",
+        default=0,
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        help="Show more output",
+        default=2,
+    )
     args = parser.parse_args(argv)
+
+    set_logger_verbosity(args.verbose - args.quiet)
     if args.force:
-        logger.info(f"Forcing serialization to {args.output_prefix}")
+        logger.debug(f"Forcing serialization to {args.output_prefix}")
     if args.model_type == "transformers":
         hf_main(args)
     elif args.model_type == "diffusers":
