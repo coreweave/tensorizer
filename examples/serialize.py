a2cf4d2623ab8ddb3729b0c34cf7f7946bb39d7d
Ben Chess <bchess@coreweave.com>
2024-03-01 13:52:09 -0800

    mmap must be MAP_SHARED

--- examples/serialize.py
+++ examples/serialize.py
@@ -14,25 +14,25 @@ model_ref = "EleutherAI/gpt-j-6B"
 # model_ref = "EleutherAI/gpt-neo-125M"
 model_name = model_ref.split("/")[-1]
 
-print('Loading model')
+print("Loading model")
 model = AutoModelForCausalLM.from_pretrained(
     model_ref,
     revision="float16",
     torch_dtype=torch.float16,
     low_cpu_mem_usage=True,
 )
-print('Done loading. To Cuda')
-model = model.to('cuda')
+print("Done loading. To Cuda")
+model = model.to("cuda")
 
 tracer = None
-if os.environ.get('VIZTRACER'):
+if os.environ.get("VIZTRACER"):
     tracer = viztracer.VizTracer(pid_suffix=True)
     tracer.start()
 start = time.perf_counter()
-print('Serializing')
-fd = os.open('/scratch/out', os.O_RDWR | os.O_CREAT)
+print("Serializing")
+fd = os.open("/scratch/out", os.O_RDWR | os.O_CREAT)
 try_fallocate(fd, 0, 12219504910)
-m = mmap.mmap(fd, 12219504910, flags=mmap.MAP_PRIVATE)
+m = mmap.mmap(fd, 12219504910, flags=mmap.MAP_SHARED)
 # m = '/scratch/out'
 serializer = TensorSerializer(m)
 serializer.write_module(model)
@@ -42,4 +42,4 @@ if tracer is not None:
     tracer.stop()
     tracer.save()
 
-print(f'Serializing took {(end-start):.3f}s')
+print(f"Serializing took {(end-start):.3f}s")
